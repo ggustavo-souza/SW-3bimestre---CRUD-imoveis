b0fe7431d2dc65efded220535a6b2ab65bb39f2e
ggustavo-souza <cavalogames1231@gmail.com>
2024-09-17 06:00:08 -0300

    comeco do header

--- templates/header.php
+++ templates/header.php
@@ -1,1 +1,20 @@
-<?php include "../config.php"; ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Crud - Imóveis</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="bs/css/bootstrap.min.css">
+    <script src="js/main.js"></script>
+    <script src="bs/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+    <main>
+        <nav class="navbar bg-primary" data-bs-theme="dark">
+            <div class="container-fluid">
+                <h3>Imóveis</h3>
+            </div>
+        </nav>
